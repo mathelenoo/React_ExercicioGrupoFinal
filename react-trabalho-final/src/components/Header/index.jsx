39f2601ca5f13be2bdae5c2da8c0710609d270f3
GabrielnPimentel <gabrielnpimentel@hotmail.com>
2024-06-08 18:03:53 -0300

    feat: criação do header e estilos globais

---
+++ react-trabalho-final/src/components/Header/index.jsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import './style.css';
+
+function Header() {
+  return (
+    <header>
+      <nav className="container">
+        <ul className="navbar-list">
+          <li className="navbar-item">
+            <Link to={"/"}>
+              <p>Home</p>
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link to={"/adicionar"}>
+              <p>Adicionar</p>
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link>
+              <p>Editar</p>
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link>
+              <p>Deletar</p>
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link to={"/resumo/:id"}>
+              <p>Ver Resumo</p>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
--- react-trabalho-final/src/pages/Adicionar/index.jsx
+++ react-trabalho-final/src/pages/Adicionar/index.jsx
@@ -1,12 +1,12 @@
 import Header from "../../components/Header"
 
-function Home() {
+function Adicionar() {
   return (
     <div>
         <Header/>
-        <h1>Essa é a página Home</h1>
+        <h1>Essa é a página Adicionar</h1>
     </div>
   )
 }
 
-export default Home
+export default Adicionar
