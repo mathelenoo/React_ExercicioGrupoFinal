c811bca7d802818177f8888ceeeeee355017428e
LTilio <Leandro.tilio@gmail.com>
2024-06-09 15:54:31 -0300

    Feat: page editar/deletar

--- react-trabalho-final/src/routes/AppRouter.jsx
+++ react-trabalho-final/src/routes/AppRouter.jsx
@@ -3,6 +3,7 @@ import Home from "../pages/Home";
 import Resumo from "../pages/Resumo";
 import Adicionar from "../pages/Adicionar";
 import { Route, Routes } from "react-router-dom";
+import Editar from "../pages/Editar";
 
 function AppRouter() {
     return (
@@ -10,6 +11,7 @@ function AppRouter() {
             <Route path="/" element={<Home />} />
             <Route path="/resumo/:id" element={<Resumo />} />
             <Route path="/adicionar" element={<Adicionar />} />
+            <Route path="/editar/:id" element={<Editar />} />
         </Routes>
     );
 }
