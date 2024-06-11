6dbb10617a98eddc74d9c041819e5c36042436ec
GabrielnPimentel <gabrielnpimentel@hotmail.com>
2024-06-10 22:50:14 -0300

    feat: criação do footer

---
+++ react-trabalho-final/src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+function Footer() {
+  return (
+    <footer>
+        <div className="footer-container">
+            <h2>Desenvolvido pelo Grupo 6</h2>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
